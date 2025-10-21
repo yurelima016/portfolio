b2be26b721946c6231319d067ac2891a16823764
yurelima016 <yurelimasouza@gmail.com>
2025-10-20 22:05:29 -0300

    initiating interactions using Javascript

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function animation() {
-    return;
+const skillBars = document.querySelectorAll('#skills-container .skill-bar');
+let currentSkillIndex = 0;
+const animationTime = 8000;
+const pauseTime = 8000;
+const totalTimePerSkill = animationTime + pauseTime;
+
+function showNextSkill() {
+
+    skillBars[currentSkillIndex].classList.add('active');
+
+    currentSkillIndex++;
+
+    if (currentSkillIndex >= skillBars.length) {
+        currentSkillIndex = 0;
+    }
+
+    setTimeout(() => {
+        skillBars.forEach(bar => bar.classList.remove('active'));
+    }, animationTime);
 }
+
+showNextSkill();
+setInterval(showNextSkill, totalTimePerSkill);
