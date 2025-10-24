1ce9571adb2fbc74a7c50bb0992f5b45718cd052
yurelima016 <yurelimasouza@gmail.com>
2025-10-24 14:56:09 -0300

    adjusting script

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const birthDate = new Date('2004-11-16');
+const today = new Date();
+let age = today.getFullYear() - birthDate.getFullYear();
+const monthDifference = today.getMonth() - birthDate.getMonth();
+if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())){
+    age--;
+}
+document.getElementById('age').textContent = age;
+
 const skillBars = document.querySelectorAll('#skills-container .skill-bar');
 let currentSkillIndex = 0;
 const animationTime = 8000;
